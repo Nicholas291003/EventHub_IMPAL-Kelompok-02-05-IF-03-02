0be5182090fea35d12a0fec67b72311d7be8f1ba
Nicholas <juninandar77@gmail.com>
2026-01-02 13:52:44 +0700

    pakett

--- resources/views/user/profile/edit.blade.php
+++ resources/views/user/profile/edit.blade.php
@@ -14,6 +14,15 @@
                 </div>
 
                 <div class="card-body p-4">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul class="mb-0">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
                     @if(session('success'))
                     <div class="alert alert-success">{{ session('success') }}</div>
                     @endif
